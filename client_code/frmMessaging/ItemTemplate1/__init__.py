59a463a87a6886c240b6f8909b3c5c0fe8bcf252
reida23@stbedes.catholic.edu.au <reida23@stbedes.catholic.edu.au>
2026-04-23 05:14:04 +0000

    Edited frmMessaging.ItemTemplate1

--- client_code/frmMessaging/ItemTemplate1/__init__.py
+++ client_code/frmMessaging/ItemTemplate1/__init__.py
@@ -4,6 +4,7 @@ import anvil.server
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
+import anvil.tz 
 
 class ItemTemplate1(ItemTemplate1Template):
   def __init__(self, **properties):
