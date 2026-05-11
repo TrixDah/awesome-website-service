dc42fcb3fe91c2dc48d4f7039a6f01d3586b751a
luckb23@stbedes.catholic.edu.au <luckb23@stbedes.catholic.edu.au>
2026-05-11 08:33:35 +0000

    Created frmUsername; Edited frmLogin, settings, frmMessaging and standard-page.html

--- client_code/frmLogin/__init__.py
+++ client_code/frmLogin/__init__.py
@@ -9,6 +9,8 @@ import anvil.google.auth, anvil.google.drive
 from anvil.google.drive import app_files
 import anvil.users
 import time
+from anvil import alert
+from ..frmUsername import frmUsername
 
 class frmLogin(frmLoginTemplate):
   def __init__(self, **properties):
@@ -20,6 +22,19 @@ class frmLogin(frmLoginTemplate):
     if user_row is not None:
       username = user_row['Username']
       users = anvil.server.call('get_user_by_username', username)
-      print
+      enabled = users[0]['enabled']
       print(list(users))
+      if not enabled: # this is here to mitagate an anvil bug (or featue) https://anvil.works/forum/t/login-with-google-allows-user-to-login-first-time-even-though-the-new-user-accounts-can-be-used-right-away-tab-is-disabled/5506/3
+        print("user not enabled!")
+        self.lblErr.visible = True
+        return
+
+      if username is None:
+        new_username = alert(frmUsername(), large=True, buttons=[])
+
+      if new_username:
+        user_row['Username'] = new_username
+      else:
+        return
+        
       open_form('frmMessaging', current_user=username)
