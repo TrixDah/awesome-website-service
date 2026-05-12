9ccdc4eb041569099d36170e0e4e4ed1f585416c
luckb23@stbedes.catholic.edu.au <luckb23@stbedes.catholic.edu.au>
2026-05-12 04:48:17 +0000

    Edited frmLogin and frmTOS

--- client_code/frmTOS/__init__.py
+++ client_code/frmTOS/__init__.py
@@ -6,8 +6,18 @@ import anvil.users
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
-
+from anvil import alert
+import anvil.js
 
 class frmTOS(frmTOSTemplate):
     def __init__(self, **properties):
         self.init_components(**properties)
+
+        anvil.js.window.accept_tos = self.accept_tos
+        anvil.js.window.decline_tos = self.decline_tos
+
+    def accept_tos(self):
+        anvil.open_form('frmLogin', True)
+
+    def decline_tos(self):
+        alert("Declined")
