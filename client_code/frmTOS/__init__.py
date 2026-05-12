a6fbe8e15e59a95208e32df0f4c0e3076e71e1e8
luckb23@stbedes.catholic.edu.au <luckb23@stbedes.catholic.edu.au>
2026-05-12 04:33:43 +0000

    Created frmTOS; Deleted Form1

--- client_code/frmTOS/__init__.py
+++ client_code/frmTOS/__init__.py
@@ -1,4 +1,4 @@
-from ._anvil_designer import Form1Template
+from ._anvil_designer import frmTOSTemplate
 import anvil.server
 import anvil.google.auth, anvil.google.drive
 from anvil.google.drive import app_files
@@ -8,6 +8,6 @@ import anvil.tables.query as q
 from anvil.tables import app_tables
 
 
-class Form1(Form1Template):
+class frmTOS(frmTOSTemplate):
     def __init__(self, **properties):
         self.init_components(**properties)
