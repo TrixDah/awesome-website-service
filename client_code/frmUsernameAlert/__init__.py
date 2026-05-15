23a97b00656535cd1b871b7465ab1a571ca73065
luckb23@stbedes.catholic.edu.au <luckb23@stbedes.catholic.edu.au>
2026-05-15 04:38:14 +0000

    renamed some elements

--- client_code/frmUsernameAlert/__init__.py
+++ client_code/frmUsernameAlert/__init__.py
@@ -1,4 +1,4 @@
-from ._anvil_designer import frmUsernameTemplate
+from ._anvil_designer import frmUsernameAlertTemplate
 from anvil import *
 import anvil.server
 import anvil.google.auth, anvil.google.drive
@@ -9,7 +9,7 @@ import anvil.tables.query as q
 from anvil.tables import app_tables
 
 
-class frmUsername(frmUsernameTemplate):
+class frmUsernameAlert(frmUsernameAlertTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
