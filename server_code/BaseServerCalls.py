3180a08b1db262901d76ed595c65e692ad39c047
luckb23@stbedes.catholic.edu.au <luckb23@stbedes.catholic.edu.au>
2026-05-22 11:26:24 +0000

    Created UserControl; Edited BaseServerCalls

--- server_code/BaseServerCalls.py
+++ server_code/BaseServerCalls.py
@@ -284,44 +284,6 @@ def get_user_by_email(email: str):
     except anvil.tables.TableError:
         return None
 
-
-@anvil.server.callable
-def delete_user_by_username(username):
-    """
-    deletes all links to a user in a non-cascading way to ensure no null-pointers/dangling pointers
-    """
-    # NPE in python!?
-    user_row = app_tables.users.get(Username=username)
-    if not user_row:
-        return f"No user found with username '{username}'"
-
-    for chat in app_tables.chats.search():
-        participants = chat['Participants'] or []
-        if user_row not in participants:
-            continue
-
-        if chat['ChatName'] == 'General Chat':
-            chat.update(Participants=[p for p in participants if p != user_row])
-        else:
-            print(list(chat))
-            for message in app_tables.messages.search(TargetChat=chat):
-                message.delete()
-            chat.delete()
-           
-    for message in app_tables.messages.search():
-        read_by = message['ReadBy'] or []
-        if user_row in read_by:
-            message.update(ReadBy=[r for r in read_by if r != user_row])
-
-    print("messages: ")
-    for message_row in app_tables.messages.search(Sender=username):
-        print(message_row)
-        message_row.delete()
-
-    user_row.delete()
-
-    return f"User '{username}' deleted successfully."
-    
 @anvil.server.callable
 def add_username(user_row, username: str):
   user_row['Username'] = username
